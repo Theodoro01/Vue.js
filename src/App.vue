f507b60111771693e6430709a7b9248b4c8ad7b1
Theodoro01 <theodorovieira01@gmail.com>
2022-04-16 20:10:28 -0300

    Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <h1>Guia Clientes </h1>
-    <Clientes :cliente="clienteTheo" :showNumber="false"/> 
+    <h2>Cadastro: </h2>
+    <small id="Erro" v-show="deuErro">O nome é invalido, tente novamente! </small><br>
+    <input type="text" placeholder="name" v-model="nameField"><br>
+    <input type="email" placeholder="email" v-model="emailField"><br>
+    <input type="number" placeholder="phone number" v-model="numberField"><br>
+    <button @click="cadastrarUsuario"> Cadastrar</button>
+    <hr>  
+    <div v-for="cliente in clientes" :key="cliente.id">
+       <Clientes :cliente="cliente" :showNumber="true" @meDelete="deletarUsuario($event)"/>
+    </div> 
 
   </div>
 </template>
@@ -14,20 +22,57 @@ export default {
   name: 'App',
   data(){
     return {
-      clienteTheo: {
-          name: "The cjb", 
+      deuErro: false,
+      nameField: "", 
+      emailField: "", 
+      numberField: 0,
+      clientes: [
+        {
+          id: 1,
+          name: "Theo", 
           email: "theo@Dev.com", 
-          number: 98765
-      }
+          number: 42367
+        },
+        {
+          id: 2,
+          name: "isabela", 
+          email: "Isabela@Dev.com", 
+          number: 2387248572893
+        },
+        {
+          id: 3,
+          name: "Arthur", 
+          email: "Arthur@Dev.com", 
+          number: 84093207
+        },
+      ]
     }
   },
   components: {
     Clientes
+  },
+  methods:{
+    cadastrarUsuario: function(){
+      if(this.nameField === " " || this.nameField.length < 3){
+        this.deuErro = true;
+      }else{
+        this.clientes.push({name: this.nameField, email: this.emailField, number: this.numberField, id: Date.now()}),
+        this.nameField = "";
+        this.emailField = ""; 
+        this.numberField = 0;
+        this.deuErro = false;
+      }
+    },
+    deletarUsuario: function($event){
+      console.log("recebendo evento.")
+      console.log($event.clienteId)
+    }
   }
-  
 }
 </script>
 
 <style>
-
+  #Erro{
+    color: red;
+  }
 </style>
--- src/components/Clientes.vue
+++ src/components/Clientes.vue
@@ -1,11 +1,13 @@
 <template>
-    <div id="cliente">
+    <div :class="{'cliente': !isPremium, 'cliente-premium': isPremium }">
         
         <h2>Name:{{cliente.name}}</h2>
         <hr>
         <p> Email: {{cliente.email}}</p>
         <p v-if="showNumber === true">Phone number: {{cliente.number}}</p>
         <p v-else> Usuario não quer mostrar seu numero</p>
+        <button @click="mudarCor"> Mudar cor!</button>
+        <button @click="eventoDelete"> Deletar</button>
     </div>
 </template>
 
@@ -13,6 +15,8 @@
 export default{
     data(){
         return{
+
+            isPremium: false
             // name: "The cjb", 
             // email: "theo@Dev.com", 
             // number: 0,
@@ -22,14 +26,26 @@ export default{
     props: {
             cliente: Object, 
             showNumber: Boolean  
+    },
+    methods: {
+        mudarCor: function(){
+            this.isPremium = !this.isPremium; 
+        },
+        eventoDelete: function(){
+            this.$emit("meDelete",{clienteId: this.cliente.id ,msg: "OLA", component: this});
+        }
     }
+
     
 }
-</script>
-
+</script>-
 <style scoped>
-    #cliente{
+    .cliente{
         color: red;
         background-color: navajowhite;
+    }
+    .cliente-premium{
+        color: gold;
+        background-color: black;
     }
 </style>
