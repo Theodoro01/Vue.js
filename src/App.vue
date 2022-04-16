38ce5cc16c27d54ed0f453af6fd673a09a237ed3
Theodoro01 <theodorovieira01@gmail.com>
2022-04-16 16:51:23 -0300

    Vue.js

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Guia Clientes </h1>
+    <Clientes :cliente="clienteTheo" :showNumber="false"/> 
+
+  </div>
+</template>
+
+<script>
+ 
+import Clientes from './components/Clientes.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      clienteTheo: {
+          name: "The cjb", 
+          email: "theo@Dev.com", 
+          number: 98765
+      }
+    }
+  },
+  components: {
+    Clientes
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Clientes.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="cliente">
+        
+        <h2>Name:{{cliente.name}}</h2>
+        <hr>
+        <p> Email: {{cliente.email}}</p>
+        <p v-if="showNumber === true">Phone number: {{cliente.number}}</p>
+        <p v-else> Usuario não quer mostrar seu numero</p>
+    </div>
+</template>
+
+<script> 
+export default{
+    data(){
+        return{
+            // name: "The cjb", 
+            // email: "theo@Dev.com", 
+            // number: 0,
+        }
+    },
+
+    props: {
+            cliente: Object, 
+            showNumber: Boolean  
+    }
+    
+}
+</script>
+
+<style scoped>
+    #cliente{
+        color: red;
+        background-color: navajowhite;
+    }
+</style>
